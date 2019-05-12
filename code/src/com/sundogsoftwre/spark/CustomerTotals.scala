f16a6812033557577f162a64097e2fc1b073ff77
Jonathan Lovelace <kingjon3377@gmail.com>
2019-05-12 16:30:13 -0400

    Add my implementation of the first 'homework' in the Spark Scala course.

---
+++ code/src/com/sundogsoftwre/spark/CustomerTotals.scala
@@ -0,0 +1,28 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+
+object CustomerTotals {
+  def parseLine(line:String) = {
+    val fields = line.split(",")
+    val customer = fields(0)
+    val item = fields(1)
+    val price = fields(2).toFloat
+    (customer, price)
+  }
+  
+  def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "CustomerTotals")
+    val lines = sc.textFile("../SparkScala/customer-orders.csv")
+    val parsedLines = lines.map(parseLine)
+    val customerTotals = parsedLines.reduceByKey((x, y) => x + y).collect()
+    for (row <- customerTotals) {
+      val customerId = row._1
+      val total = row._2
+      println(f"Customer $customerId%s spent $total%.2f")
+    }
+  }
+}
